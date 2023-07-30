5f7ec03e2ead852f8487ee5121376e36bcb4d37f
isd-sgcu <isd.team.sgcu@gmail.com>
2023-07-30 17:57:54 +0000

    Update from https://github.com/isd-sgcu/rpkm66-proto/commit/7c57ac3877e6fd5337dd5d798b3ba09b24bf32d4

--- src/rpkm66.checkin.user.v1.rs
+++ src/rpkm66.checkin.user.v1.rs
@@ -16,6 +16,42 @@ pub struct AddEventResponse {
     #[prost(message, optional, tag = "1")]
     pub event: ::core::option::Option<super::super::event::v1::Event>,
 }
+///
+/// GetAllUserEventsByNamespace
+#[derive(serde::Deserialize, serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct GetAllUserEventsByNamespaceIdRequest {
+    #[prost(string, tag = "1")]
+    pub user_id: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub namespace_id: ::prost::alloc::string::String,
+}
+#[derive(serde::Deserialize, serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct GetAllUserEventsByNamespaceIdResponse {
+    #[prost(message, repeated, tag = "1")]
+    pub event: ::prost::alloc::vec::Vec<super::super::event::v1::UserEvent>,
+}
+///
+/// GetUserEventByEventId
+#[derive(serde::Deserialize, serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct GetUserEventByEventIdRequest {
+    #[prost(string, tag = "1")]
+    pub user_id: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub event_id: ::prost::alloc::string::String,
+}
+#[derive(serde::Deserialize, serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct GetUserEventByEventIdResponse {
+    #[prost(message, optional, tag = "1")]
+    pub user_event: ::core::option::Option<super::super::event::v1::UserEvent>,
+}
 /// Generated client implementations.
 pub mod user_service_client {
     #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
@@ -128,6 +164,66 @@ pub mod user_service_client {
                 );
             self.inner.unary(req, path, codec).await
         }
+        pub async fn get_all_user_events_by_namespace_id(
+            &mut self,
+            request: impl tonic::IntoRequest<super::GetAllUserEventsByNamespaceIdRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::GetAllUserEventsByNamespaceIdResponse>,
+            tonic::Status,
+        > {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/rpkm66.checkin.user.v1.UserService/GetAllUserEventsByNamespaceId",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new(
+                        "rpkm66.checkin.user.v1.UserService",
+                        "GetAllUserEventsByNamespaceId",
+                    ),
+                );
+            self.inner.unary(req, path, codec).await
+        }
+        pub async fn get_user_event_by_event_id(
+            &mut self,
+            request: impl tonic::IntoRequest<super::GetUserEventByEventIdRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::GetUserEventByEventIdResponse>,
+            tonic::Status,
+        > {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/rpkm66.checkin.user.v1.UserService/GetUserEventByEventId",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new(
+                        "rpkm66.checkin.user.v1.UserService",
+                        "GetUserEventByEventId",
+                    ),
+                );
+            self.inner.unary(req, path, codec).await
+        }
     }
 }
 /// Generated server implementations.
@@ -142,6 +238,20 @@ pub mod user_service_server {
             request: tonic::Request<super::AddEventRequest>,
         ) -> std::result::Result<
             tonic::Response<super::AddEventResponse>,
+            tonic::Status,
+        >;
+        async fn get_all_user_events_by_namespace_id(
+            &self,
+            request: tonic::Request<super::GetAllUserEventsByNamespaceIdRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::GetAllUserEventsByNamespaceIdResponse>,
+            tonic::Status,
+        >;
+        async fn get_user_event_by_event_id(
+            &self,
+            request: tonic::Request<super::GetUserEventByEventIdRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::GetUserEventByEventIdResponse>,
             tonic::Status,
         >;
     }
@@ -253,6 +363,101 @@ pub mod user_service_server {
                     let fut = async move {
                         let inner = inner.0;
                         let method = AddEventSvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.unary(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/rpkm66.checkin.user.v1.UserService/GetAllUserEventsByNamespaceId" => {
+                    #[allow(non_camel_case_types)]
+                    struct GetAllUserEventsByNamespaceIdSvc<T: UserService>(pub Arc<T>);
+                    impl<
+                        T: UserService,
+                    > tonic::server::UnaryService<
+                        super::GetAllUserEventsByNamespaceIdRequest,
+                    > for GetAllUserEventsByNamespaceIdSvc<T> {
+                        type Response = super::GetAllUserEventsByNamespaceIdResponse;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::Response>,
+                            tonic::Status,
+                        >;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<
+                                super::GetAllUserEventsByNamespaceIdRequest,
+                            >,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                (*inner).get_all_user_events_by_namespace_id(request).await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let inner = inner.0;
+                        let method = GetAllUserEventsByNamespaceIdSvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.unary(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/rpkm66.checkin.user.v1.UserService/GetUserEventByEventId" => {
+                    #[allow(non_camel_case_types)]
+                    struct GetUserEventByEventIdSvc<T: UserService>(pub Arc<T>);
+                    impl<
+                        T: UserService,
+                    > tonic::server::UnaryService<super::GetUserEventByEventIdRequest>
+                    for GetUserEventByEventIdSvc<T> {
+                        type Response = super::GetUserEventByEventIdResponse;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::Response>,
+                            tonic::Status,
+                        >;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<super::GetUserEventByEventIdRequest>,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                (*inner).get_user_event_by_event_id(request).await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let inner = inner.0;
+                        let method = GetUserEventByEventIdSvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
