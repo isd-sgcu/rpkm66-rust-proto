ffb8997556fe29c1e0e026a9a62d8378c2badabe
isd-sgcu <isd.team.sgcu@gmail.com>
2023-08-02 12:44:29 +0000

    Update from https://github.com/isd-sgcu/rpkm66-proto/commit/d59dd69f4457f71257d9efafaf24625346174419

--- src/rpkm66.backend.user.v1.rs
+++ src/rpkm66.backend.user.v1.rs
@@ -210,6 +210,23 @@ pub struct GetUserEstampResponse {
     #[prost(message, repeated, tag = "1")]
     pub event_list: ::prost::alloc::vec::Vec<super::super::event::v1::Event>,
 }
+/// Update Personality Game
+#[derive(serde::Deserialize, serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct UpdatePersonalityGameRequest {
+    #[prost(string, tag = "1")]
+    pub id: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub personality_game: ::prost::alloc::string::String,
+}
+#[derive(serde::Deserialize, serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct UpdatePersonalityGameResponse {
+    #[prost(message, optional, tag = "1")]
+    pub user: ::core::option::Option<User>,
+}
 /// Generated client implementations.
 pub mod user_service_client {
     #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
@@ -542,6 +559,36 @@ pub mod user_service_client {
                 );
             self.inner.unary(req, path, codec).await
         }
+        pub async fn update_personality_game(
+            &mut self,
+            request: impl tonic::IntoRequest<super::UpdatePersonalityGameRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::UpdatePersonalityGameResponse>,
+            tonic::Status,
+        > {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/rpkm66.backend.user.v1.UserService/UpdatePersonalityGame",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new(
+                        "rpkm66.backend.user.v1.UserService",
+                        "UpdatePersonalityGame",
+                    ),
+                );
+            self.inner.unary(req, path, codec).await
+        }
     }
 }
 /// Generated server implementations.
@@ -612,6 +659,13 @@ pub mod user_service_server {
             request: tonic::Request<super::GetUserEstampRequest>,
         ) -> std::result::Result<
             tonic::Response<super::GetUserEstampResponse>,
+            tonic::Status,
+        >;
+        async fn update_personality_game(
+            &self,
+            request: tonic::Request<super::UpdatePersonalityGameRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::UpdatePersonalityGameResponse>,
             tonic::Status,
         >;
     }
@@ -1083,6 +1137,52 @@ pub mod user_service_server {
                     let fut = async move {
                         let inner = inner.0;
                         let method = GetUserEstampSvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.unary(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/rpkm66.backend.user.v1.UserService/UpdatePersonalityGame" => {
+                    #[allow(non_camel_case_types)]
+                    struct UpdatePersonalityGameSvc<T: UserService>(pub Arc<T>);
+                    impl<
+                        T: UserService,
+                    > tonic::server::UnaryService<super::UpdatePersonalityGameRequest>
+                    for UpdatePersonalityGameSvc<T> {
+                        type Response = super::UpdatePersonalityGameResponse;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::Response>,
+                            tonic::Status,
+                        >;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<super::UpdatePersonalityGameRequest>,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                (*inner).update_personality_game(request).await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let inner = inner.0;
+                        let method = UpdatePersonalityGameSvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
